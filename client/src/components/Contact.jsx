8f8eadb817ecc3c0429d813237cd910cb7ee9d00
Thomas <thomas@feka.nl>
2023-06-19 17:10:46 +0200

    Het is weer mooi geweest

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const Contact = () => {
   return (
     <section className='w-screen h-screen flex justify-center items-center'>Contact</section>
--- client/src/pages/Class.jsx
+++ client/src/pages/Class.jsx
@@ -31,13 +31,11 @@ const Class = () => {
     return <p>Un momento de su precioso tiempo por favor ..</p>;
   }
 
-  console.log(data);
   return (
     <>
       <main className="w-screen h-screen flex flex-col">
         <div className="w-screen h-[95px] bg-black text-white flex justify-center items-center">
-          AVV BARRIO DE LA CRUZ
-        </div>
+          
         <Link
           className="self-center bg-black text-white rounded-3xl p-2 m-2 hover:text-blue-500"
           to="/"
@@ -45,22 +43,23 @@ const Class = () => {
           {" "}
           Volver al inicio
         </Link>
-        <header className="w-[40%] h-8 bg-gray-500 text-white mx-auto flex items-center justify-center rounded-3xl text-3xl p-6">
+          AVV BARRIO DE LA CRUZ
+        </div>
+       
+        <header className="w-[40%] h-8 bg-gray-500 text-white mx-auto flex items-center justify-center rounded-3xl text-3xl p-6 m-2">
           Clase de {data.name}
         </header>
 
         <div class="w-full h-full flex mt-12 justify-center gap-9">
-          <div className="flex flex-col h-full">
+          
+         <div className="flex flex-col ">
+            <article className="shadow-lg p-6 rounded-full w-[350px] h-[350px] flex items-center justify-center">Description: {data.description}</article>
+            <article className="shadow-lg p-6 rounded-full w-[350px] h-[350px] flex items-center justify-center">Informacion general: <ul><li>{data.teacher}</li></ul> </article>
+          </div> 
 
-          <section className="p-5 border-0 border-neutral-800 rounded-full w-[50%] h-[50%]flex items-center justify-center shadow-lg" >{data.description}</section>
-        
-            <section className="p-5 border-0 border-neutral-800 rounded-full w-[50%]  h-[50%] flex items-center justify-center shadow-lg">
-              <h2>informacion importante:</h2>
-            </section>
-          </div>
-
-
-            <section className="p-5 border-0 border-neutral-800 rounded-full w-[20%] h-full flex items-center justify-center shadow-lg">3</section>
+          <article>
+            
+          </article>
      
         </div>
 
